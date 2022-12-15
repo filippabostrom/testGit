30724ad105a181a45876312ea31c21d2e9e2fe30
idawiden <idawideen@hotmail.com>
2022-12-15 13:19:15 +0100

    hej

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -15,7 +15,12 @@ public class TestGit {
      */
     public static void main(String[] args) {
        System.out.println("hej på dig");
+    
+    System.out.println ("hejhej");
+    
     }
+    
+    
    
     
 }
